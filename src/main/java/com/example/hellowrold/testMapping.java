dd5b3b7886afcf23a21a08b9783137c0438b76af
墨 <1813242837@qq.com>
2024-03-04 14:42:46 +0800

    1

---
+++ src/main/java/com/example/hellowrold/testMapping.java
@@ -0,0 +1,29 @@
+package com.example.hellowrold;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+public class testMapping {
+    @PostMapping
+    public void save()
+    {
+
+    }
+    @DeleteMapping
+    public  void  delete()
+    {
+
+    }
+    @PutMapping
+    public  void update()
+    {
+
+    }
+    @GetMapping
+    public void list()
+    {
+
+    }
+}
